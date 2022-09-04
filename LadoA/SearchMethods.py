8acdb2d22cd78e9e13888ce4672c015c4b27f81e
Joaquín García del Río <jogarcia@itba.edu.ar>
2022-09-04 20:43:09 -0300

    Some comments deleted

--- LadoA/SearchMethods.py
+++ LadoA/SearchMethods.py
@@ -295,15 +295,4 @@ class A:
             #   print("Estado repetido/visitado")
             state = self.get_min_from_f()
             self.path.append(state)
-            
-            
-            
-            
-    ##
-    ##
-    #    3 4 0 3 1   4 4 0 3 1   0 0 0 3 1
-    #    4 3 1 1 3   4 3 1 1 3   0 3 1 1 3 
-    #    1 2 2 1 3   1 2 2 1 3
-    #    0 1 3 3 2   0 1 3 3 2
-    ##   1 3 0 0 4   1 3 0 0 4
-    #
+
